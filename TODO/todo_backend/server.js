8709b4d622f5aa8b6acbe246e831ea4255f1e9e7
aditya47050 <163160914+aditya47050@users.noreply.github.com>
2026-03-22 13:58:55 +0530

    initial change

---
+++ TODO/todo_backend/server.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const TodoModel = require('./models/Todo');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: ${process.env.PORT}`);
+});
+
+app.post('/add', (req, res) => {
+  const { task } = req.body;
+
+  TodoModel.create({ task })
+  .then(result => res.json(result))
+  .catch(err => console.log(err));
+});
+
+app.get('/get', (req,res)=>{
+  TodoModel.find()
+  .then(result => res.json(result))
+  .catch(err => console.log(err));
+});
+
+app.put('/edit/:id',(req,res)=>{
+  const { id } = req.params;
+
+  TodoModel.findByIdAndUpdate(id,{done:true},{new:true})
+  .then(result => res.json(result))
+  .catch(err => res.json(err));
+});
+
+app.put('/update/:id',(req,res)=>{
+  const { id } = req.params;
+  const { task } = req.body;
+
+  TodoModel.findByIdAndUpdate(id,{task:task})
+  .then(result => res.json(result))
+  .catch(err => res.json(err));
+});
+
+app.delete('/delete/:id',(req,res)=>{
+  const { id } = req.params;
+
+  TodoModel.findByIdAndDelete({_id:id})
+  .then(result => res.json(result))
+  .catch(err => res.json(err));
+});
+
+module.exports = app;
